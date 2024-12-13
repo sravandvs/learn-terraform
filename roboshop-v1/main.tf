e4003f2658a98e5444c08fcefdaf50653376208e
sravandvs <sravandvs6@gmail.com>
2024-12-14 00:19:28 +0530

    terraform

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-08ad53375d8c94295"]
+  tags = {
+    Name = "frontend"
+  }
 }
