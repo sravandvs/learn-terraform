e536e6ca845a61668c9504bd40c9ed2d94f3a74a
sravandvs <sravandvs6@gmail.com>
2024-12-19 18:35:15 +0530

    variables

---
+++ roboshop_count_loop/main.tf
@@ -0,0 +1,26 @@
+variable "instances" {
+  default = [
+    "frontend",
+    "cart",
+    "catalogue",
+    "user",
+    "shipping",
+    "payment",
+    "mysql",
+    "mongodb",
+    "rabbitmq",
+    "redis",
+    "dispatch"
+
+  ]
+}
+
+resource "aws_instance" "instance" {
+  count         = length(var.instances)
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-08ad53375d8c94295"]
+  tags = {
+    Name = var.instances[count.index]
+  }
+}
