341920b054b8fb9cbcd180576474fa1fc3d90e52
sravandvs <sravandvs6@gmail.com>
2024-12-19 15:43:55 +0530

    variables

--- 02-variables/main.tf
+++ 02-variables/main.tf
@@ -9,3 +9,45 @@ output "URL"{
 output "URL1" {
   value = "URL - ${URL}"
 }
+
+variable "num1" {
+  default = 10
+}
+
+variable "str1" {
+  default = "Hello"
+}
+
+variable "str2" {
+  default = "World"
+}
+
+variable "list1" {
+  default = [
+  10,
+  20,
+  "xyz",
+  false
+  ]
+}
+
+variable "map1"{
+  default = {
+    aws   = {
+      trainer = "John"
+      duration = 30
+    }
+    devops = {
+      trainer = "Steve"
+      duration = 20
+    }
+  }
+}
+
+output "list_1"{
+  value = var.list1[2]
+}
+
+output "map1_aws_trainer"{
+  value = var.map1["aws"]["trainer"]
+}
