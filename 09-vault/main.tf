2a790929cf6d78286ea077ddccc0b31d8587787d
sravandvs <sravandvs6@gmail.com>
2025-01-31 16:27:27 +0530

    Hashicorp Vault

--- 09-vault/main.tf
+++ 09-vault/main.tf
@@ -10,13 +10,13 @@ data "vault_generic_secret" "example" {
   path = "test/my_credentials"
 }
 
-data "vault_kv_secret_v2" "example" {
- name = "my_credentials"
- mount = "test"
-}
+# data "vault_kv_secret_v2" "example" {
+# name = "my_credentials"
+# mount = "test"
+# }
 
 resource "local_file" "foo" {
-  content  = data.vault_kv_secret_v2.example.data_json["password"]
+  content  = data.vault_generic_secret.example.data["password"]
   filename = "/tmp/secret"
 }
 
